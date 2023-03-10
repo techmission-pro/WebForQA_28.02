b249f6ed0a01cf0c4c93bd1bd71dfc2dfad1c9f3
Zhminkovska <Zhminkovska.i@gmail.com>
2023-03-10 22:13:55 +0200

    calculate

--- resources/calc.js
+++ resources/calc.js
@@ -1,12 +1,26 @@
 document.querySelector('.calc-calculate').addEventListener('click', function () {
     const num1 = +document.querySelector('[name="num1"]').value;
     const num2 = +document.querySelector('[name="num2"]').value;
+	const op = document.querySelector('[name="op"]').value;
+	
+    let result;
 
-    const sum = num1 + num2;
+    if	(op === 'plus'){
+		result = num1 + num2;
+	} else if (op === 'minus'){
+		result = num1 - num2;
+	} else if (op === 'multiply'){
+		result = num1 * num2;
+	} else if (op === 'divide'){
+		result = num1 / num2;
+	} else {
+		console.error('Wrong op!');
+		alert ('Wrong operation is selected');
+	}
+		   	
+    document.querySelector('.calc-result').value = result;
 	
-    document.querySelector('.calc-result').innerText = sum;
-	
-});	
+	});	
 
 
 
