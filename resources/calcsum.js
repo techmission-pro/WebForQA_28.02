9321274ca43cd65d58a35f9c4958a48e58842e40
Michael <mishamon@ukr.net>
2023-03-12 18:16:50 +0200

    Task 4 step 3

--- resources/calcsum.js
+++ resources/calcsum.js
@@ -1,5 +1,4 @@
-//console.log('calc loaded');
-document.querySelector('.calc-calculate').addEventListener('click', function() {
+function calculate() {
 	const num1 = +document.querySelector('[name="num1"]').value;
 	const num2 = +document.querySelector('[name="num2"]').value;
 	const op = document.querySelector('[name="op"]').value;
@@ -13,11 +12,20 @@ document.querySelector('.calc-calculate').addEventListener('click', function() {
 	} else if (op === 'divide') {
 				result = num1 / num2;
 	}
-	
-	
-	
-	
-//	const sum = num1 + num2;
-	
+
 	document.querySelector('.calc-result').value = result;
+}
+
+
+document.querySelector('.calc-calculate').addEventListener('click', function() {
+	calculate();
+});
+document.querySelector('[name="op"]').addEventListener('change', function() {
+	calculate();
+});
+document.querySelector('[name="num1"]').addEventListener('input', function() {
+	calculate();
+});
+document.querySelector('[name="num2"]').addEventListener('input', function() {
+	calculate();
 });
