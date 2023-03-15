b85332b737e90830b82a65925f2e5848dcab7e9b
AfanasievK <afkm0310@gmail.com>
2023-03-15 21:35:02 +0200

    Added cod qa.html

--- resources/calc.js
+++ resources/calc.js
@@ -1,1 +1,8 @@
-console.log('Calc loaded');
+document.querySelector('.calc-calculate').addEventListener('clic' function () {
+	const nul1 = +document.querySelector('[name="num1"]').value;
+	const nul1 = +document.querySelector('[name="num1"]').value;
+
+	const sum = num1 + num2;
+
+	document.querySelector('.calc-result').value = sum;
+});
