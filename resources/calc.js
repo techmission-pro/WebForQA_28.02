104d20b3724c078453171db24e140a28f03dc9c1
Yan <yan.ahnevskiy@gmail.com>
2023-03-18 14:12:50 +0200

    the final form of the calculator

--- resources/calc.js
+++ resources/calc.js
@@ -1,9 +1,10 @@
-document.querySelector('.calc-calculate').addEventListener('click',function () {
+function calculate() {
     const num1 = +document.querySelector('[name="num1"]').value;
 	const num2 = +document.querySelector('[name="num2"]').value;
 	const op = document.querySelector('[name="op"]').value;
 
     let result;
+	
 	if (op === 'plus') {
 		result = num1 + num2;
 	} else if (op === 'minus') {
@@ -18,4 +19,18 @@ document.querySelector('.calc-calculate').addEventListener('click',function () {
 	}	
 		
 	document.querySelector('.calc-result').value = result;
+}
+
+document.querySelector('.calc-calculate').addEventListener('click', function() {
+	calculate();
+});	
+
+document.querySelector('[name="op"]').addEventListener('change', function() {
+	calculate();
 });
+document.querySelector('[name="num1"]').addEventListener('input', function() {
+	calculate();
+})
+document.querySelector('[name="num2"]').addEventListener('input', function() {
+	calculate();
+})
