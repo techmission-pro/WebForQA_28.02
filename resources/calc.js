eb4a6223f2215ab9dcc796a1cd1c95606d983902
Kolisnyk <kolivan09@gmail.com>
2023-03-19 13:59:21 +0200

    Calc3

---
+++ resources/calc.js
@@ -0,0 +1,24 @@
+function calculate (){
+const num1 = +document.querySelector('[name="num1"]').value;
+const num2 = +document.querySelector('[name="num2"]').value;
+const op = document.querySelector('[name="op"]').value;
+
+let result;
+
+if (op === 'plus') {
+    result = num1 + num2;}
+	else if (op === 'minus') {
+	result = num1 - num2;}
+	else if (op === 'multiply') {
+	result = num1 * num2;}
+	else if (op === 'divide') {
+	result = num1 / num2;}
+	else { console.error('Wrong op!');
+	alert('Wrong operation is seleoted');}
+	
+document.querySelector('.calc-result').value = result;
+}
+
+document.querySelector('.calc-calculate').addEventlistener('click', function () {
+    calculate();
+});
