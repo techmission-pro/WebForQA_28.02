7b9174479af43c9cc2bf1f70c8e87c42dd581d49
Nikita Yevmeniev <nikita.evmeniev@gmail.com>
2023-03-19 22:22:23 +0000

    Add name and surnname

--- resources/calc.js
+++ resources/calc.js
@@ -1,18 +1,21 @@
-document.querySelector(".calc-calculate").addeventListenier("click", function () {
-	counst num1 =+document.querySelector('[name="num1"]).value;
-	counst num2 =+document.querySelector('[name="num2"]).value;
-	counst op=document.querySelector('[name="op"].value;
-
-let result;
-
-if [op === 'plus']  }
-result = num1 + num2;
-} else if [op === 'minus'] {
-result = num1 - num2;
-} else if(op === 'multiply'] {
+document.querySelector(".calc-calculate").addEventListenier('click', function () {
+	const num1 =+document.querySelector('[name="num1"]).value;
+	const num2 =+document.querySelector('[name="num2"]).value;
+	const op= document.querySelector('[name="op"]).value;
+	
+	let result;
+	
+if (op === 'plus') {
+	result = num1 + num2;
+} else if (op === 'minus') {
+	result = num1 - num2;
+} else if(op === 'multiply') {
  result = num1 * num2;
- ] else{
+} else if(op === 'divide') {
    result = num1 / num2;
+   } else {
+	   console.error('Wrong op!');
+	   alert('Wrong operation is selected';
    }
-document.querySelector('.calc-result').innerText = sum;
+document.querySelector('.calc-result').value = result;
 }};
